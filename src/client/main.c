4a80d255bc23356737f3f6e1a5a5c743e6df3034
dennis.musk <395181939@qq.com>
2015-05-19 14:55:16 +0800

    add client pipe

--- src/client/main.c
+++ src/client/main.c
@@ -2,19 +2,45 @@
 
 #include <proto.h>
 
+
+static int pd[2];
+
 /*
  *  -P receive port
  *  -M multipe group
  *  -p play name
  *  -H help
  */
-int main()
+int main(int argc, char **argv)
 {
+	pid_t pid;
 
 	/* initialize */
+	if (pipe[pd] < 0) {
+		perror("pipe()");
+		exit(1);
+	}
+
+	pid = fork();
+	if (pid < 0) {
+		perror("fork()");
+		exit(1);
+	} else (pid == 0) {
+		close(pd[1]); /* close write */
+		dup2(pd[0], 0);
+		if (pd[0] > 0) {
+			close(pd[0]);
+		}
+
+		/* child, run decorder */
+		execl("/usr/bin/mpg123", "mpg123", "-"/* stdin/stdout */, NULL);
+		exit(0);
+	}
+
+	/* parent */
+	close(pd[0]);
 
 	/* receive menulist */
-
 	while (1) {
 		if ("is menulist?") {
 			break;
@@ -22,10 +48,10 @@ int main()
 	}
 
 	/* select channel */
-
 	while (1) {
 		if ("is the chanel you want ?") {
 			/* play */
+			write(pd[1],);
 		}
 	}
 
