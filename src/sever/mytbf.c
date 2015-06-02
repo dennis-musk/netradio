91dc710fe371ad169454813fd9c5e7c13e1c4101
dennis.musk <395181939@qq.com>
2015-06-02 11:23:39 +0800

    finish mytbf

--- src/sever/mytbf.c
+++ src/sever/mytbf.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <errno.h>
+#include <syslog.h>
 
 #include "mytbf.h"
 
@@ -12,22 +14,20 @@ struct mytbf_st {
 	int burst;
 	int token;
 	pthread_mutex_t mut;
-	pthread_con_t cond;
+	pthread_cond_t cond;
 };
 
 static struct mytbf_st *tbf[TBFMAX];
 
 static pthread_mutex_t mut_tbf = PTHREAD_MUTEX_INITIALIZER;
 
-
-static pthread_t tid;
+static pthread_t tid_timer;
 static pthread_once_t init_once = PTHREAD_ONCE_INIT;
 
 static void *thr_timer(void *unused)
 {
 	int i;
 	struct timespec t;
-
 
 	while (1) {
 		t.tv_sec = 1;
@@ -40,26 +40,37 @@ static void *thr_timer(void *unused)
 		}
 
 		for (i = 0; i < TBFMAX; ++i) {
-			if (tbuf[i]) {
-				tbf[i]->token += tbuf[i]->cps;
+			if (tbf[i]) {
+				pthread_mutex_lock(&tbf[i]->mut);
+				tbf[i]->token += tbf[i]->cps;
 				if (tbf[i]->token > tbf[i]->burst) {
 					tbf[i]->token = tbf[i]->burst;
 				}
+
+				pthread_cond_broadcast(&tbf[i]->cond);
+				pthread_mutex_unlock(&tbf[i]->mut);
 			}
 		}
 	}
 }
 
+static void module_unload(void)
+{
+	pthread_cancel(tid_timer);
+	pthread_join(tid_timer, NULL);
+}
 
 static void module_load(void)
 {
 	int err;
 
-	err = pthread_create(&tid, NULL, thr_timer, NULL);
+	err = pthread_create(&tid_timer, NULL, thr_timer, NULL);
 	if (err) {
 		syslog(LOG_ERR, "pthread_create(): %m");
 		exit(1);
 	}
+
+	atexit(module_unload);
 }
 
 
@@ -95,15 +106,15 @@ mytbf_t *mytbf_init(int cps, int burst)
 	me->token = 0;
 
 	pthread_mutex_init(&me->mut, NULL);
-	pthread_cond_int(&me->cond, NULL);
+	pthread_cond_init(&me->cond, NULL);
 
 	pthread_mutex_lock(&mut_tbf);
 	pos = get_free_pos();
 	if (pos < 0) {
 		pthread_mutex_unlock(&mut_tbf);
 		free(me);
-		phread_cond_destroy(&me->mut);
-		phread_mutex_destroy(&me->mut);
+		pthread_cond_destroy(&me->cond);
+		pthread_mutex_destroy(&me->mut);
 		return NULL;
 	}
 
@@ -114,18 +125,47 @@ mytbf_t *mytbf_init(int cps, int burst)
 	return me;
 }
 
-int mytbf_destroy(mytbf_t *)
+int mytbf_destroy(mytbf_t *ptr)
 {
+	struct mytbf_st *me = ptr;
 
+	pthread_cond_destroy(&me->cond);
+	pthread_mutex_destroy(&me->mut);
+
+	free(me);
+
+	return 0;
 }
 
-int mytbf_fetchtoken(mytbf_t *, int n)
+static int min(int a, int b)
 {
-
+	return  a > b ? a : b;
 }
 
-int mytbf_returntoken(mytbf_t *, int n)
+int mytbf_fetchtoken(mytbf_t *ptr, int n)
 {
+	int token;
+	struct mytbf_st *me = ptr;
 
+	pthread_mutex_lock(&me->mut);
+	while (me->token <= 0) {
+		pthread_cond_wait(&me->cond, &me->mut);
+	}
+	token = min(me->token, n);
+	me->token -= token;
+	pthread_mutex_unlock(&me->mut);
+
+	return token;
+}
+
+int mytbf_returntoken(mytbf_t *ptr, int n)
+{
+	struct mytbf_st *me = ptr;
+
+	pthread_mutex_lock(&me->mut);
+	me->token += n;
+	pthread_cond_broadcast(&me->cond);
+
+	pthread_mutex_unlock(&me->mut);
 }
 
